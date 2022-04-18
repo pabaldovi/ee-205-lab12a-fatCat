37a7ff9b0ee5a9748df6dc6f6c83ee121b8454c1
paulobaldovi <baldovipaulo@gmail.com>
2022-04-17 22:04:40 -1000

    Defined public member functions

--- Weight.cpp
+++ Weight.cpp
@@ -25,3 +25,87 @@ static const float SLUGS_IN_A_POUND = 0.031081;
 static const string POUND_LABEL = "Pound";
 static const string KILO_LABEL = "Kilo";
 static const string SLUG_LABEL = "Slug";
+
+bool Weight::isWeightKnown() const noexcept {
+    return weight == UNKNOWN_WEIGHT;
+}
+
+bool Weight::hasMaxWeight() const noexcept {
+    return maxWeight == UNKNOWN_WEIGHT;
+}
+
+float Weight::getWeight() const noexcept {
+    return weight;
+}
+
+float Weight::getWeight(Weight::UnitOfWeight weightUnits) const noexcept {
+    return convertWeight(getWeight(), getWeightUnit(), weightUnits);
+}
+
+float Weight::getMaxWeight() const noexcept {
+    return maxWeight;
+}
+
+Weight::UnitOfWeight Weight::getWeightUnit() const noexcept {
+    return unitOfWeight;
+}
+
+void Weight::setWeight(float newWeight) {
+    assert(isWeightValid(newWeight));
+    weight = newWeight;
+}
+
+void Weight::setWeight(float newWeight, Weight::UnitOfWeight weightUnits) {
+    assert(isWeightValid(newWeight));
+    weight = newWeight;
+    unitOfWeight = weightUnits;
+}
+
+bool Weight::isWeightValid(float checkWeight) const noexcept {
+    assert(checkWeight > 0);
+    if (bHasMax) {
+        assert(checkWeight <= maxWeight);
+    }
+    return true;
+}
+
+bool Weight::validate() const noexcept {
+    assert(isWeightValid(weight));
+    assert(isWeightValid(maxWeight));
+    return true;
+}
+
+void Weight::dump() const noexcept {
+    cout << setw(80) << setfill( '=' ) << "" << endl ;
+    cout << setfill( ' ' ) ;
+    cout << left ;
+    cout << boolalpha ;
+    FORMAT_LINE( "Weight", "this" ) << this << endl;
+    FORMAT_LINE( "Weight", "isKnown" ) << isWeightKnown() << endl;
+    FORMAT_LINE( "Weight", "weight" ) << weight << endl;
+    FORMAT_LINE( "Weight", "unitOfWeight" ) << getWeightUnit() << endl;
+    FORMAT_LINE( "Weight", "hasMax" ) << hasMaxWeight() << endl;
+    FORMAT_LINE( "Weight", "maxWeight" ) << maxWeight << endl;
+}
+
+bool Weight::operator==(const Weight &rhs_Weight) const {
+    /// Remember to convert the two weight's units into a common unit!
+    /// Treat unknown weights as 0 (so we can sort them without dealing
+    /// with exceptions)
+    float lhs_weight = (bIsKnown) ? getWeight(Weight::POUND) : 0;
+    float rhs_weight = (rhs_Weight.bIsKnown) ?
+                       rhs_Weight.getWeight(Weight::POUND) : 0;
+    return lhs_weight == rhs_weight;
+}
+
+bool Weight::operator<(const Weight &rhs_Weight) const {
+    float lhs_weight = (bIsKnown) ? getWeight(Weight::POUND) : 0;
+    float rhs_weight = (rhs_Weight.bIsKnown) ?
+                       rhs_Weight.getWeight(Weight::POUND) : 0;
+    return lhs_weight < rhs_weight;
+}
+
+Weight &Weight::operator+=(float rhs_addToWeight) {
+    weight += rhs_addToWeight;
+    return *this;
+}
