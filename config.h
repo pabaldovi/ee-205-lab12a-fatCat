b221bec8748808328422eb20946b4c8fd78dcaf5
paulobaldovi <baldovipaulo@gmail.com>
2022-04-17 22:27:47 -1000

    Added config.h

---
+++ config.h
@@ -0,0 +1,14 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee_205_lab12a_fatCat - EE 205 - Spr 2022
+///
+/// @file config.h
+/// @version 1.0
+///
+/// @author Paulo Baldovi <pbaldovi@hawaii.edu>
+/// @date   17_Apr_2022
+///////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#define PROGRAM_NAME "Fat Cat"
